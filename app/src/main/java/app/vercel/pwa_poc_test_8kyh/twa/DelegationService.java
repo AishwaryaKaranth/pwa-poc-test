b1e8ad70eeef9b56649309756d80ca675c21aa60
AishwaryaKaranth <aishwaryakaranth27899@gmail.com>
2023-02-28 17:03:57 +0530

    added andriod apk

---
+++ app/src/main/java/app/vercel/pwa_poc_test_8kyh/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.pwa_poc_test_8kyh.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
